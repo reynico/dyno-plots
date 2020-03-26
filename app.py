8157b03b94feeab65c04a44494178631371b2600
Nico Rey <rey.nico@gmail.com>
2020-03-26 18:38:46 -0300

    Seems like RPM string tag depends on MWD software version

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def parse_mwd(root):
             for nombre in root2.iter('Nombre'):
                 root3 = etree.Element('root')
                 root3 = nombre
-                if root3.text == "RPM Motor":
+                if root3.text == "RPM Motor" or root3.text == "RPM Motor Filtrada":
                     for muestras in root2.iter('Muestra'):
                         rpm_samples = muestras.text.split(", ")
                 if root3.text == "Torque Corr":
